d0684fef18c082b1803c152854887025cdd37067
jasonthaisy <jtsy@uci.edu>
2018-04-23 16:53:05 -0700

    finished template

--- src/com/garbagecollector/GCSimulator.java
+++ src/com/garbagecollector/GCSimulator.java
@@ -1,13 +1,23 @@
 package com.garbagecollector;
+import java.io.*;
+import java.util.*;
 
 public class GCSimulator {
+    private HashMap<String, Object> stackRef;
+    // type 1 reference: stack-heap, a stack variable containing a
+    // pointer that points to a heap object
+
+    private HashMap<Object, Map<String, Object>> heapRef;
+    // type 2 reference: heap-heap, where a field of an object containing
+    // a pointer that points to another heap object
 
     public GCSimulator() {
-
+        stackRef = new HashMap<String, Object>();
+        heapRef = new HashMap<Object, Map<String, Object>>();
     }
 
     static void assign(String aName, String bName, Object o) {
-
+        //add pair <aName, o> into the stackRef.
     }
 
     // aName and bName represent the variable names “a” and “b”, respectively;
@@ -18,7 +28,7 @@ public class GCSimulator {
 
     // aName is the name of variable “a”, and o represents the newly created object
     static void readObject (String bName, String aName,  Object o) {
-
+        //add pair <bName, o> into the stackRef.
     }
 
     // bName and aName represent the two variables “b” and “a”,
