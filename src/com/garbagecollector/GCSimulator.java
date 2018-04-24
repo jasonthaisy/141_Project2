c05e2dd25e1de50b6bdf3957288e7a174bb3c2de
jasonthaisy <jtsy@uci.edu>
2018-04-24 16:21:24 -0700

    more progress

--- src/com/garbagecollector/GCSimulator.java
+++ src/com/garbagecollector/GCSimulator.java
@@ -22,13 +22,22 @@ public class GCSimulator {
     }
 
     static void assign(String aName, String bName, Object o) {
-        //B c = b;
-        //GCSimulator.assign("c", "b", b);
-
-        //assign Object o to aName (Object o is associated with bName) ?? idk
-
         //add pair <aName, o> into the stackRef.
         stackRef.put(aName, o);
+
+        //add all map entries associated with o to a new HashMap.
+        HashMap<String, Object> associatedRefs = new HashMap<String, Object>();
+
+        if (stackRef.get(bName) != null)
+            associatedRefs.put(bName, stackRef.get(bName));
+
+        for (Map.Entry<String, Object> e : stackRef.entrySet()) {
+            if (o.equals(e.getValue()) && !associatedRefs.containsKey(e.getKey()))
+                associatedRefs.put(e.getKey(), e.getValue());
+        }
+
+        //add this HashMap associatedRefs into the heapRef, with Object o
+        heapRef.put(o, associatedRefs);
     }
     // aName and bName represent the variable names “a” and “b”, respectively;
     // o denotes the object referenced by a and b
@@ -39,14 +48,20 @@ public class GCSimulator {
     }
     // aName is the name of variable “a”, and o represents the newly created object
 
+    //todo: debug readObject if anything, all other methods (except writeObject as of now) seem right
     static void readObject (String bName, String aName,  Object o) {
         //add pair <bName, o> into the stackRef.
+        stackRef.put(bName, o);
+        //heapRef.put(o, new HashMap<String, Object>(stackRef)); //not sure
+        //possibly other things?
     }
     // bName and aName represent the two variables “b” and “a”,
     // and o denotes the heap object referenced by b
 
     static void writeObject(String aName, String bName,  String fieldname, Object oa, Object ob) {
-
+        //when a field write is seen, we need to perform the following map update:
+        //( (Map)heapRef.get(oa) ).put(fieldname, ob), which basically replaces
+        //the old object referenced by the field "fieldname" of oa w/ a new obj ob.
     }
     // bName and aName represent the two variables “b” and “a”,
     // fieldname denotes the name of the field f,
