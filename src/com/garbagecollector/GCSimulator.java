c6f890a515bacc22af9b0729ab29e8bcecbc216f
jasonthaisy <jtsy@uci.edu>
2018-04-24 18:16:39 -0700

    part 1 almost done

--- src/com/garbagecollector/GCSimulator.java
+++ src/com/garbagecollector/GCSimulator.java
@@ -23,13 +23,12 @@ public class GCSimulator {
 
     static void assign(String aName, String bName, Object o) {
         //add pair <aName, o> into the stackRef.
+        if (stackRef.get(aName) != null)
+            heapRef.remove(stackRef.get(aName));
         stackRef.put(aName, o);
-
         //add all map entries associated with o to a new HashMap.
         HashMap<String, Object> associatedRefs = new HashMap<String, Object>();
-
-        if (stackRef.get(bName) != null)
-            associatedRefs.put(bName, stackRef.get(bName));
+        associatedRefs.put(aName, stackRef.get(bName));
 
         for (Map.Entry<String, Object> e : stackRef.entrySet()) {
             if (o.equals(e.getValue()) && !associatedRefs.containsKey(e.getKey()))
@@ -42,9 +41,14 @@ public class GCSimulator {
     // aName and bName represent the variable names “a” and “b”, respectively;
     // o denotes the object referenced by a and b
 
-    static void createObject (String aName, Object o) {
+    static void createObject(String aName, Object o) {
         allObjects.add(o);
         stackRef.put(aName, o);
+
+        HashMap<String, Object> init = new HashMap<String, Object>();
+        init.put(aName, o);
+
+        heapRef.put(o, init);
     }
     // aName is the name of variable “a”, and o represents the newly created object
 
@@ -62,6 +66,9 @@ public class GCSimulator {
         //when a field write is seen, we need to perform the following map update:
         //( (Map)heapRef.get(oa) ).put(fieldname, ob), which basically replaces
         //the old object referenced by the field "fieldname" of oa w/ a new obj ob.
+        //stackRef.put(fieldname, ob); //todo: might not acct for all cases so come back here as well
+
+        heapRef.get(oa).put(fieldname, ob);
     }
     // bName and aName represent the two variables “b” and “a”,
     // fieldname denotes the name of the field f,
@@ -71,11 +78,35 @@ public class GCSimulator {
     static void gc() {
         //first, identify root objects, i.e. objects pointed to by stack variables
         //traverse stackRef and find such objects
+        HashSet<Object> roots = new HashSet<Object>();
+        for (Map.Entry<String, Object> e : stackRef.entrySet()) {
+            roots.add(e.getValue());
+        }
 
         //use a graph traversal algorithm that iteratively identifies transitively
         //reachable objects by chasing references in map heapRef
-    }
+        HashMap<Object, Boolean> reachable = new HashMap<Object, Boolean>();
+        for (Map.Entry<Object, Map<String, Object>> e : heapRef.entrySet()) {
+            for (Map.Entry<String, Object> e2 : e.getValue().entrySet()) {
+                reachable.put(e2.getValue(), true);
+            }
+        }
 
+        //print results
+        boolean allReached = true;
+        System.out.println("The following objects are unreachable:");
+
+        for (Object o : allObjects) {
+            if (reachable.get(o) == null) {
+                System.out.println("Object " + o.toString());
+                allReached = false;
+            }
+        }
+
+        if (allReached) {
+            System.out.println("All items were reached.");
+        }
+    }
     // a gc method that will be explicitly called to collect garbage
 
 }
--- src/com/garbagecollector/Main.java
+++ src/com/garbagecollector/Main.java
@@ -3,6 +3,17 @@ package com.garbagecollector;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello world!");
+	    GCSimulator simulator = new GCSimulator();
+
+	    Object a = new Object();
+	    Object b = new Object();
+	    Object c = b;
+
+	    simulator.createObject("a", a);
+	    simulator.createObject("b", b);
+	    simulator.assign("c", "b", b);
+	    //simulator.writeObject("c", "a", "f", c, a);
+        //simulator.assign("a", "b", b);
+        simulator.gc();
     }
 }
